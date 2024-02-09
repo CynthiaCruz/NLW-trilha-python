9aa678ad00e759d4571107beb46c988fdead4057
Cynthia Cruz <cynthia.cruzoliveira@gmail.com>
2024-02-09 17:37:00 -0300

    parte de arquitetura finalizada

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRquest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import CreateTagController
 
 class TagCreatorView:
     '''
@@ -7,7 +8,11 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRquest) -> HttpResponse:
+        tag_creator_controller = CreateTagController()
+
         body = http_request.body
         product_code = body["product_code"]
 
-        return HttpResponse(status_code=200, body={"hello": "word"})
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
